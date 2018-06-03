f439253f0fac1d9b70db12b6de6909a976db7c2f
Yonatan Betzer <yonatan.betzer@gmail.com>
2018-06-03 12:26:56 +0300

    Initial commit

---
+++ app/src/main/java/com/yonatanbetzer/redditapp/activities/RedditLobby.java
@@ -0,0 +1,17 @@
+package com.yonatanbetzer.redditapp.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.yonatanbetzer.redditapp.R;
+
+public class RedditLobby extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reddit_lobby);
+
+        // https://www.reddit.com/r/ludobots/comments/4lctij/new_start_here/
+    }
+}
---
+++ app/src/main/java/com/yonatanbetzer/redditapp/adapters/RedditLobbyAdapter.java
@@ -0,0 +1,23 @@
+package com.yonatanbetzer.redditapp.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class RedditLobbyAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
